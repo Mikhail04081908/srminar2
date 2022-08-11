2987d08d2bc784dfdbc29c1c8d59a74557d62438
Kaluzhin <fk2090@gmail.com>
2022-08-11 15:02:01 +0300

    homework

---
+++ ex_001/Program.cs
@@ -0,0 +1,7 @@
+﻿// Программа, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.Clear();
+
+int num = new Random().Next(100, 1000);
+string num1 = num.ToString();
+Console.WriteLine($"Число: {num}");
+Console.Write($"Вторая цифра числа: {num1[1]}");
---
+++ ex_002/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Clear();
+
+int num = new Random().Next(10, 1000);
+int lastDigit = num % 10;
+
+if(num < 100)
+{
+    Console.Write($"Число {num} - двузначное, третьего числа не существует. ");
+}
+else
+{
+    Console.WriteLine($"Число: {num}");
+    Console.Write($"Третья цифра {lastDigit}");
+}
